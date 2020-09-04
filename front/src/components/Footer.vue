512431f1103958d5f49ddce6c571fa06ebc5b39e
Karol Harumi <karol.harumi@hotmail.com>
2020-09-03 22:52:59 -0600

    feat: story bubbles flow

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="footer">
-    <div class="phone-viewport">
-      <md-bottom-bar md-type="shift">
-        <md-bottom-bar-item to="/home" exact id="bottom-bar-item-home" md-label="Home" md-icon="home"></md-bottom-bar-item>
-        <md-bottom-bar-item to="/teste" exact id="bottom-bar-item-pages" md-label="Perfil" md-icon="face"></md-bottom-bar-item>
-        <md-bottom-bar-item to="/story" exact id="bottom-bar-item-posts" md-label="Favoritos" md-icon="favorite"></md-bottom-bar-item>
-        <md-bottom-bar-item to="/register" exact id="bottom-bar-item-favorites" md-label="Mais" md-icon="more_horiz"></md-bottom-bar-item>
-      </md-bottom-bar>
-    </div>
+  <div class="footer">  
+    <md-bottom-bar md-type="shift">
+      <md-bottom-bar-item to="/home" exact id="bottom-bar-item-home" md-label="Home" md-icon="home"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/teste" exact id="bottom-bar-item-pages" md-label="Perfil" md-icon="face"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/story" exact id="bottom-bar-item-posts" md-label="Favoritos" md-icon="favorite"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/register" exact id="bottom-bar-item-favorites" md-label="Mais" md-icon="more_horiz"></md-bottom-bar-item>
+    </md-bottom-bar>
   </div>
+
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
   .footer {
     position: fixed;
     bottom: 0;
@@ -20,4 +19,9 @@
     left: 0;
     right: 0;
   }
+
+  .footer .md-ripple {
+    justify-content: center;
+  }
+
 </style>
--- front/src/views/Story.vue
+++ front/src/views/Story.vue
@@ -1,21 +1,71 @@
 <template>
   <div class="story">
-    <div class="book">
-      <div class="left-page">
-        <h1>Lenda da Vitória Régia</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mauris nunc, rhoncus non tellus sit amet, rhoncus maximus dui. Nullam fermentum mattis tellus a interdum.</p>
-        <p>Vestibulum quam dolor, porta et nulla ultricies, pretium blandit velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In vitae ante magna. Etiam semper, mauris ut eleifend cursus, diam risus efficitur diam, nec rutrum metus ex eget massa. Aliquam scelerisque metus id dictum porta. Fusce accumsan libero ac accumsan condimentum. Donec non ex turpis.</p>
-      </div>
-      <div class="right-page">
-        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec maximus iaculis nisl, vel iaculis justo dictum eu. Sed eu tincidunt nisi, ac vestibulum ipsum. Aliquam consectetur euismod purus a sollicitudin. Nam quis dui felis. Aliquam erat volutpat. Phasellus dapibus lectus ac tincidunt rhoncus. </p>
-        <p>Integer vestibulum libero hendrerit ex elementum varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin rhoncus fermentum viverra. In consequat, urna eget dictum molestie, nunc magna gravida felis, at sodales elit leo sed elit. In hac habitasse platea dictumst. Proin lobortis ante et odio pharetra pharetra.</p>
-        <p>Pellentesque suscipit lacus non mauris venenatis scelerisque. Donec erat velit, vestibulum ut leo a, elementum placerat eros. Donec lacinia arcu tellus. Sed lobortis porttitor risus at imperdiet. Pellentesque rhoncus sem eu augue facilisis, id condimentum nunc mollis. Sed sit amet bibendum lectus. Quisque in mi nec justo sagittis sodales.</p>
-      </div>
+    <div class="nav">
+      <h1>Era uma vez um Safari: </h1>
+      <md-button class="md-raised md-primary" @click="addComicBubbles()">Iniciar História</md-button>
+      <md-button class="md-raised md-primary" @click="destroy()">Finalizar</md-button>
+    </div>
+  
+    <div class="image">
+      <img id="kids-safari" src="../assets/bg_story.jpg" width="100%">
     </div>
   </div>
 </template>
 
-<style>
+<script>
+
+export default {
+  methods: {
+
+    addComicBubbles() {
+      window.CharactersBubbles = new window.ComicBubbles("kids-safari", {canvas: {readonly: false}}, {bubble: [
+        {id: 'b1', text: "Diálogo 1...", x: 884.8, y: 223.73, width: 194, height: 69, fontSize: '30px', background: '#fff', color: '#000', bubbleStyle: 'speak', tailLocation: 'e', tailX: 1920.72, tailY: 506.06, visible: false},
+        {id: 'b2', text: "Fala do Narrador...", className: 'lion', lineHeight: 1, x: 2500, y: 50, width: 'auto', height: 'auto', fontSize: '30px', background: '#6b8e23', color: '#ffffff', bubbleStyle: 'none', tailLocation: 's', tailX: 533, tailY: 177, visible: false}
+      ]});
+
+      window.CharactersBubbles.onCanvasLoad(function(){
+        var Bubble1 = this.getBubbleById('b1'); 
+        var Bubble2 = this.getBubbleById('b2');
+        
+        //bubbles appear and disappear with callback functions
+          Bubble1.setClassName('lioness').delay(1000).setWidth('auto').setHeight('auto').pumpUp(function(){
+            Bubble2.delay(1000).fadeIn(function(){
+              Bubble1.delay(2000).fadeOut();
+
+              //'this' refers to Bubble2
+              this.delay(2000).fadeOut(function(){ 
+                Bubble1.delay(1000).setText("Diálogo 2\n...").pumpUp(function(){
+                  Bubble2.delay(1000).fadeIn(function(){
+                    Bubble1.delay(2000).fadeOut();
+                    
+                    this.delay(2000).fadeOut(function(){
+                      Bubble1.delay(1000).setFontSize(25).setText("Diálogo 3...").fadeIn(function(){
+                        Bubble2.delay(1000).setText("CRAC!").setFontWeight('bold').setX(483).setY(54).setTailX(531).setTailY(178).setBackground('#ff0000').pumpUp(100,function(){								
+                          Bubble2.delay(2000).fadeOut(function(){
+                            this.remove();
+                            Bubble1.moveTo(2554,837,'fast').setText("Diálogo 4...");
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+      });
+    },
+
+    destroy(){
+      window.DestroyComicBubbles(window.CharactersBubbles);
+    },
+
+  }
+
+}
+</script>
+
+<style scoped>
   .story {
     font-family: 'Roboto', sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -23,36 +73,18 @@
     color: #2c3e50;
     min-height: 100vh;
     background-color: #fff;
-    padding: 0 5%;
   }
 
-  .book {
-    width: 100%;
-    height: 100vh;
-    background: url('../assets/bg_book.jpg') no-repeat center/cover;
+  .story .nav {
     display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-    align-items: stretch;
-    align-content: stretch;
-    padding: 5%;
-    font-size: 16px;
-    line-height: 1.5;
-  }
-
-  .book .left-page {
-    width: 50%;
-    margin-right: 8%;
-  }
-
-  .book .right-page {
-    width: 50%;
+    justify-content: center;
+    align-items: center;
+  
   }
 
   h1 {
     line-height: 1.3;
-    margin-bottom: 0;
+    margin: 0;
     font-family: 'Grandstander', cursive;
     font-weight: 900;
     font-size: 31px;
