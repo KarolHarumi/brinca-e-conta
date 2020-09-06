45e1953f60c9f571a8da9ee9c002caea6ebacce2
Karol Harumi <karol.harumi@hotmail.com>
2020-09-06 16:56:28 -0600

    feat: choice during the story

--- front/src/views/Story.vue
+++ front/src/views/Story.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="story">
-    <!--<div class="choice-modal hide">
-      <p>Que susto! Ainda bem que essa plantinha ajudou a Naiá a chegar a superfície!</p>
+    <div class="choice-modal hide" :style="{'background-image': 'url(' + require(`../assets/vitoria-regia/story_vr_choice_${choiceBackground}.png`) + ')'}">
+
+      <p class="intro">{{ choiceText }}</p>
 
       <div class="question">
-        <p>Vamos também ajudar a Naiá e escolher onde a planta pode deixar a indiazinha em segurança!</p>
-        <span class="button-primary" @click="getChoice()">Na grama verdinha</span>
-        <span class="button-primary" @click="getChoice()">Ao lado de uma árvore florida</span>
+        <p>{{ choiceQuestion }}</p>
+        <span class="button-primary" @click="getChoice()">{{ choiceAnswers[0] }}</span>
+        <span class="button-primary" @click="getChoice()">{{ choiceAnswers[1] }}</span>
       </div>
-    </div>-->
-
-    <div class="nav" @click="startStory()">
-      <h1>Lenda da Vitória Régia</h1>
-      <h2>Olá {{ name }}!</h2>
-      <p>Preparada para deixar nossa historinha ainda mais divertida?</p>
     </div>
-  
-    <div id="containerScenario">
-      <img :src="require(`../assets/vitoria-regia/story_vr_${index}.png`)" alt="Lenda da Vitória Régia" />
-      <span class="dialog hide" :style="(`${position}: 20px`)">
-        <p>{{ sentences }}</p>
-      </span>
+
+    <div class="containerScenario">
+      <div class="nav" @click="startStory()">
+        <h1>Lenda da Vitória Régia</h1>
+        <h2>Olá {{ name }}!</h2>
+        <p>Preparada para deixar nossa historinha ainda mais divertida?</p>
+      </div>
+
+      <div id="scenario">
+        <img :src="require(`../assets/vitoria-regia/story_vr_${index}.png`)" alt="Lenda da Vitória Régia" />
+        <span class="dialog hide" :style="(`${position}`)">
+          <p>{{ sentences }}</p>
+        </span>
+      </div>
     </div>
 
   </div>
@@ -29,14 +32,19 @@
 <script>
 
 export default {
-  
+
   data: function() {
       return {
         name: `${localStorage.getItem('name')}`,
         age: `${localStorage.getItem('age')}`,
         index: 0,
         sentences: '',
-        position: 'bottom'
+        position: 'bottom: 20px',
+
+        choiceText: '',
+        choiceQuestion: '',
+        choiceAnswers: '',
+        choiceBackground: 0
       }
   },
 
@@ -46,125 +54,119 @@ export default {
       document.querySelector('.dialog').classList.remove('hide');
       document.querySelector('.nav').classList.add('hide');
 
-      this.storyNavigate();  
+      this.storyNavigate();
     },
 
     storyNavigate() {
       const dialogs = [
         {
           sentence: '',
-          hasChoice: false
-        },
-        { 
-          sentence: 'Há quem diga que muitos anos\n atrás um forte indiozinho havia se\n transformado em estrela.',
-          hasChoice: false,
-          position: 'top'
+          position: 'top: 25%'
         },
         {
-          sentence: 'E esta estrela foi crescendo,\n crescendo dando origem a Lua.',
-          hasChoice: false,
-          position: 'bottom'
+          sentence: 'Há quem diga que muitos anos atrás um forte indiozinho havia se transformado em estrela.',
+          position: 'top: 25%'
         },
         {
-          sentence: 'Alguns não acreditam nessa história,\n mas outros sim, como a\n indiazinha Naiá.',
-          hasChoice: false,
-          position: 'top'
+          sentence: 'E esta estrela foi crescendo, crescendo dando origem a Lua.',
+          position: 'top: 45%'
         },
         {
-          sentence: 'Naiá era uma amante da natureza.\n Cuidava de rosas e plantas com\n muito carinho e amor. E adorava se\n enfeitar com elas.',
-          hasChoice: true,
-          choice: {
+          sentence: 'Alguns não acreditam nessa história, mas outros sim, como a indiazinha Naiá.',
+          position: 'top: 60px'
+        },
+        {
+          sentence: 'Naiá era uma amante da natureza. Cuidava de rosas e plantas com muito carinho e amor. E adorava se enfeitar com elas.',
+          position: 'top: 20px',
+        },
+        {
+          sentence: 'Ela sempre foi conhecida em sua aldeia por ser apaixonada pela luz da lua. Mal podia esperar que o sel se retirasse para que a noite chegasse e então ela pudesse contemplar a luz do luar.',
+          position: 'bottom: 20px',
+          choices: {
+            text: 'Como a indiazinha Naiá é bonita!',
+            question: 'Como ela deveria enfeitar os cabelos?',
+            answers: ['Com tranças', 'Com arquinho'],
+            background: 0,
+          }
+        },
+        {
+          sentence: 'Até que em um dia de lua cheia ela notou de sua oca que o brilho da lua estava mais intenso e saiu para contempla-lo.',
+          position: 'bottom: 15%'
+        },
+        {
+          sentence: 'Chegando até a beira do rio avistou um facho de luz sobre a água. Nesse momento se apoiou sobre uma pedra para chegar mais perto.',
+          position: 'top: 20px',
+          choices: {
             text: 'Vamos ajudar a indiazinha Naiá!',
             question: 'Por qual caminho ela precisa ir pra ver a luz da lua?',
-            alternatives: ['Na beira do rio', 'Dentro da floresta']
-          },
-          position: 'top'
+            answers: ['Na beira do rio', 'Dentro da floresta'],
+            background: 1,
+          }
         },
         {
-          sentence: 'Ela sempre foi conhecida em sua\n aldeia por ser apaixonada pela\n luz da lua. Mal podia esperar que o sel se retirasse para que a noite chegasse e então ela pudesse contemplar a luz do luar.',
-          hasChoice: false,
-          position: 'bottom'
+          sentence: 'Foi aí que viu a figura de um belo índio refletido na água vindo da Luz da Lua.',
+          position: 'top: 50px'
         },
         {
-          sentence: 'Até que em um dia de lua cheia ela\n notou de sua oca que o brilho da\n lua estava mais intenso e saiu\n para contempla-lo.',
-          hasChoice: true,
-          choice: {
-            text: 'Vamos ajudar a indiazinha Naiá!',
-            question: 'Por qual caminho ela precisa ir pra ver a luz da lua?',
-            alternatives: ['Na beira do rio', 'Dentro da floresta']
-          },
-          position: 'bottom'
-        },
-        {
-          sentence: 'Chegando até a beira do rio avistou\n um facho de luz sobre a água. Nesse momento se apoiou sobre\n uma pedra para chegar mais perto.',
-          hasChoice: false,
-          position: 'top'
-        },
-        {
-          sentence: 'Foi aí que viu a figura de um belo\n índio refletido na água vindo da\n Luz da Lua.',
-          hasChoice: true,
-          choice: {
+          sentence: 'A indiazinha se encanta pelo índio, e num ato de impulso, tenta dar um beijo na imagem refletida.',
+          position: 'bottom: 15%',
+          choices: {
             text: 'Nossa, a indiazinha Naiá é bem curiosa e acabou vendo o rosto de alguém muito bonito!',
             question: 'Chegou a hora de escolher! O que a Naiá vai fazer?',
-            alternatives: ['Ela fica encantada com o indiozinho!', 'Ela vai tentar dar um beijo na imagem']
-          },
-          position: 'top'
+            answers: ['Ela fica encantada com o indiozinho!', 'Ela vai tentar dar um beijo na imagem'],
+            background: 2,
+          }
         },
         {
-          sentence: 'A indiazinha se encanta pelo índio,\n e num ato de impulso, tenta dar\n um beijo na imagem refletida.',
-          hasChoice: false,
-          position: 'bottom'
+          sentence: 'Nesse momento ela se desequilibra, cai no rio e vai afundando, desacordada.',
+          position: 'top: 22%'
         },
         {
-          sentence: 'Nesse momento ela se desequilibra,\n cai no rio e vai afundando,\n desacordada.',
-          hasChoice: false,
-          position: 'bottom'
+          sentence: 'Até que um facho de luz atinge algumas plantas, raízes e algas do rio que despertam e se entrelaçam envolvendo Naiá.',
+          position: 'top: 22%',
         },
         {
-          sentence: 'Até que um facho de luz atinge\n algumas plantas, raízes e algas do\n rio que despertam e se entrelaçam\n envolvendo Naiá.',
-          hasChoice: true,
-          choice: {
+          sentence: 'E uma planta formada por uma grande bolha em forma de círculo e com bordas levantadas começou a erguer Naiá até a superfície.',
+          position: 'bottom: 50px',
+          choices: {
             text: 'Que susto! Ainda bem que essa plantinha ajudou a Naiá a chegar a superfície!',
-            question: 'Vamos também ajudar a Naiá e escolher onde a planta pode deixar a indiazinha em segurança!',
-            alternatives: ['Na grama verdinha', 'Ao lado de uma árvore florida']
-          },
-          position: 'top'
+            question: 'Vamos escolher onde a planta pode deixar a indiazinha em segurança!',
+            answers: ['Na grama verdinha', 'Ao lado de uma árvore florida'],
+            background: 3,
+          }
         },
         {
-          sentence: 'E uma planta formada por uma grande bolha em\n forma de círculo e com bordas levantadas\n começou a erguer Naiá até a superfície.',
-          hasChoice: true,
-          position: 'bottom'
+          sentence: 'Esta planta é conhecida como Vitória Régia. Desde então, a indiazinha passou a ser chamada pelo nome da famosa planta amazônica.',
+          position: 'top: 50px'
         },
         {
-          sentence: 'Esta planta é conhecida como\n Vitória Régia. Desde então, a\n indiazinha passou a ser chamada\n pelo nome da famosa planta\n amazônica.',
-          hasChoice: false,
-          position: 'top'
+          sentence: 'Flutuando nas beiras dos rios sobre uma grande raíz redonda. A Vitória Régia é muito caridosa e distribui plantas para os animais da floresta.',
+          position: 'bottom: 10%'
         },
         {
-          sentence: 'Flutuando nas beiras dos rios\n sobre uma grande raíz redonda.\n A Vitória Régia é muito caridosa e distribui\n plantas para os animais da floresta.',
-          hasChoice: false,
-          position: 'bottom'
+          sentence: 'O suco extraído de suas raízes é utilizado como tintura negra para os cabelos e durante a noite...',
+          position: 'bottom: 10%'
         },
         {
-          sentence: 'O suco extraído de suas raízes é\n utilizado como tintura negra para\n os cabelos e durante a noite...',
-          hasChoice: false,
-          position: 'bottom'
-        },
-        {
-          sentence: '...flores\n brancas e perfumadas florescem de sua\n raíz enquanto contempla a luz da\n lua e o brilho das estrelas.',
-          hasChoice: false,
-          position: 'top'
+          sentence: '...flores brancas e perfumadas florescem de sua raíz enquanto contempla a luz da lua e o brilho das estrelas.',
+          position: 'top: 25%'
         }
       ];
 
-      document.querySelector('body').addEventListener('click', () => {
+      document.querySelector('.containerScenario').addEventListener('click', () => {
         const totalDialogs = dialogs.length;
         this.index++;
 
-        // if(dialogs[this.index].hasChoice) {
-        //   console.log('haschoice');
-        //   document.querySelector('.choice-modal').classList.remove('hide');
-        // }
+        let current = dialogs[this.index]
+
+        if (current.choices) {
+          this.choiceText = current.choices.text;
+          this.choiceQuestion = current.choices.question;
+          this.choiceAnswers = current.choices.answers;
+          this.choiceBackground = current.choices.background;
+
+          document.querySelector('.choice-modal').classList.remove('hide');
+        }
 
         if(this.index <= (totalDialogs - 1)) {
           this.sentences = dialogs[this.index].sentence;
@@ -172,15 +174,15 @@ export default {
         } else {
             this.index = totalDialogs;
         }
-      
-      });
-    }
 
-    // getChoice() {
-    //   var selected = document.querySelector('.button-primary').textContent;
-    //   document.querySelector('.choice-modal').classList.add('hide');
-    //   console.log('selected', selected);
-    // }
+      });
+    },
+
+    getChoice() {
+      var selected = document.querySelector('.button-primary').textContent;
+      document.querySelector('.choice-modal').classList.add('hide');
+      console.log('Alternativa selecionada: ', selected);
+    }
   }
 }
 </script>
@@ -236,41 +238,65 @@ export default {
     max-width: 70%;
   }
 
-
-  /* modal choice */
+  /* choice */
   .hide {
     display: none!important;
   }
 
   .choice-modal {
-    position: fixed;
+    position: absolute;
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
     z-index: 10;
+    background-size: cover;
     background-color: #fff;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
-    justify-content: space-around;
+    padding: 0 40px;
     font-family: 'Fredoka One', cursive;
-    font-size: 18px;
     height: 100vh;
+    color: #F7F536;
+    font-size: 20px;
   }
 
-  #containerScenario {
+  .choice-modal .intro {
+    color: #fff;
+    margin: 0;
+    height: 190px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .choice-modal .question {
+    margin-top: 20%;
+  }
+
+  .choice-modal .button-primary {
+    display: block;
+    background-color: #92CA27;
+    color: #fff;
+    border-radius: 20px;
+    padding: 10px 35px;
+    box-shadow: 0 2px 0 2px #165330;
+    font-size: 21px;
+    font-family: 'Fredoka One', cursive;
+    border: 0;
+    text-align: center;
+    margin-top: 25px;
+  }
+
+  /*  */
+  #scenario {
     position: relative;
   }
 
-  #containerScenario img {
+  #scenario img {
     width: 100%;
     height: 100vh;
   }
 
-  #containerScenario .dialog {
+  #scenario .dialog {
     position: absolute;
     left: 20px;
     right: 20px;
@@ -280,23 +306,5 @@ export default {
     color: #fff;
     font-size: 18px;
     line-height: 1.3;
-  }
-
-  /*TODO: CRIAR COMPONENTE*/
-  .button-primary {
-    display: block;
-    background-color: #ECBF2B;
-    color: #fff;
-    border-radius: 20px;
-    padding: 10px 35px;
-    box-shadow: 0 2px 0 2px #62BBE0;
-    font-size: 21px;
-    font-family: 'Fredoka One', cursive;
-    border: 0;
-    text-align: center;
-  }
-
-  .button-primary + .button-primary {
-    margin-top: 20px;
   }
 </style>
