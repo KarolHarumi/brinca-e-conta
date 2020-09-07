d2b9b552cd3cba5e6bd3d511b3f621c4970b9a1e
Daniela Rocha Silva <=>
2020-09-07 14:24:10 -0300

    feat: finish home implementation

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -7,76 +7,41 @@
       <div class="container">
         <div class="avatar"></div>
         <div class="greetings">
-          <p class="welcome">
+          <h2 class="welcome">
             Bem vindo, <span class="user-name">{{ name }}</span>
-          </p>
-          <span class="disclaimer">
+          </h2>
+          <p class="disclaimer">
             Aqui você pode escolher qual é a sua próxima aventura! Vamos juntos?
-          </span>
+          </p>
         </div>
       </div>
     </div>
 
-    <div class="cards-container">
-      <md-card>
-        <md-card-header>
-          <md-card-header-text>
-            <div class="md-title">Lenda da Vitória Régia</div>
-            <div class="md-subhead">Nome do Autor</div>
-          </md-card-header-text>
-
-          <md-card-media md-big>
-            <img src="../assets/books/cover_mock.png" alt="Capa" />
-          </md-card-media>
-        </md-card-header>
-
-        <md-card-actions>
-          <router-link to="/story" class="md-dense md-raised md-primary">
-            <md-button>Ler</md-button>
-          </router-link>
-          <md-button>Favoritar</md-button>
-        </md-card-actions>
-      </md-card>
-
-      <md-card>
-        <md-card-header>
-          <md-card-header-text>
-            <div class="md-title">Lenda da Vitória Régia</div>
-            <div class="md-subhead">Nome do Autor</div>
-          </md-card-header-text>
-
-          <md-card-media md-big>
-            <img src="../assets/books/cover_mock.png" alt="Capa" />
-          </md-card-media>
-        </md-card-header>
-
-        <md-card-actions>
-          <router-link to="/story" class="md-dense md-raised md-primary">
-            <md-button>Ler</md-button>
-          </router-link>
-          <md-button>Favoritar</md-button>
-        </md-card-actions>
-      </md-card>
-
-      <md-card>
-        <md-card-header>
-          <md-card-header-text>
-            <div class="md-title">Lenda da Vitória Régia</div>
-            <div class="md-subhead">Nome do Autor</div>
-          </md-card-header-text>
-
-          <md-card-media md-big>
-            <img src="../assets/books/cover_mock.png" alt="Capa" />
-          </md-card-media>
-        </md-card-header>
-
-        <md-card-actions>
-          <router-link to="/story" class="md-dense md-raised md-primary">
-            <md-button>Ler</md-button>
-          </router-link>
-          <md-button>Favoritar</md-button>
-        </md-card-actions>
-      </md-card>
+    <div class="stories">
+      <section class="custom">
+        <h3 class="title">Historinhas para você</h3>
+        <div class="container -horizontal-scroll">
+          <button class="story-card -boitata"></button>
+          <button class="story-card -boto-rosa"></button>
+        </div>
+      </section>
+      <section class="finished">
+        <h3 class="title">Seus livros lidos</h3>
+        <div class="container -horizontal-scroll">
+          <button class="story-card -vitoria-regia-finished"></button>
+        </div>
+      </section>
+      <section class="categories">
+        <h3 class="title">Categorias</h3>
+        <div class="container">
+          <button class="category-card -tales"></button>
+          <button class="category-card -sci-fi"></button>
+          <button class="category-card -fantasy"></button>
+          <button class="category-card -legends-myths"></button>
+          <button class="category-card -juvenile"></button>
+          <button class="category-card -mystery-police-terror"></button>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -100,11 +65,16 @@ export default {
 </script>
 
 <style>
+.home {
+  font-family: "Fredoka One", cursive;
+  font-size: 16px;
+  margin-bottom: 2em;
+}
+
 .home .profile {
   display: flex;
   flex-flow: column;
   align-items: center;
-  font-family: "Fredoka One", cursive;
   padding: 0 2em;
 }
 
@@ -112,7 +82,7 @@ export default {
   display: flex;
   flex-flow: column;
   align-items: center;
-  padding: 2em 0;
+  padding: 1.8em 0;
   box-sizing: border-box;
   border-bottom: 1px solid #d8d9d9;
 }
@@ -144,20 +114,110 @@ export default {
   margin: 0 2em;
 }
 
-.cards-container {
+.home .stories {
+  padding: 0.8em 2em 2em 2em;
+}
+
+.stories .title {
+  color: #6f6f6f;
+  font-size: 1em;
+  margin: 1.2em 0;
+}
+
+.stories .container {
+  padding: 0 0 1em;
   display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
+  flex-wrap: wrap;
   justify-content: space-between;
-  align-items: stretch;
-  align-content: stretch;
 }
 
-.md-card {
-  margin-bottom: 5%;
+.stories .container.-horizontal-scroll {
+  overflow-x: scroll;
+  overflow-y: hidden;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
 }
 
-.md-card:last-child {
-  margin-bottom: 25%;
+.stories .story-card {
+  min-width: 243px;
+  height: 82px;
+  background-color: #102f11;
+  box-shadow: 0px 5px 0px 0px #2f7b23;
+  margin: 0 1em;
+  border-radius: 13px;
+  position: relative;
+}
+
+.stories .finished .story-card {
+  /* opacity: 0.7; */
+  margin-bottom: 30px;
+}
+
+.stories .story-card.-boitata {
+  background: url("../assets/story-cards/boitata.png") no-repeat top/cover;
+}
+
+.stories .story-card.-boto-rosa {
+  background: url("../assets/story-cards/boto_rosa.png") no-repeat top/cover;
+}
+
+.stories .story-card.-vitoria-regia-finished {
+  background: url("../assets/story-cards/vitoria_regia_finished.png") no-repeat
+    top/cover;
+}
+
+.stories .finished .story-card:after {
+  content: "Completo";
+  position: absolute;
+  bottom: -24px;
+  text-align: right;
+  left: 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  z-index: -1;
+  background-color: #2f7b23;
+  width: 100%;
+  height: 30px;
+  border-bottom-left-radius: 13px;
+  border-bottom-right-radius: 13px;
+  font-family: "Fredoka One", cursive;
+  font-size: 0.9em;
+  color: #f7f536;
+  padding: 0.5em 1.3em;
+}
+
+.stories .categories .category-card {
+  width: 82px;
+  height: 82px;
+  background-color: #f0faff;
+  box-shadow: 0px 3px 0px 0px #78d6fd;
+  margin: 1em 0;
+  border-radius: 20px;
+}
+
+.stories .categories .category-card.-tales {
+  background: url("../assets/categories/tales.png") no-repeat top/cover;
+}
+
+.stories .categories .category-card.-sci-fi {
+  background: url("../assets/categories/sci-fi.png") no-repeat top/cover;
+}
+
+.stories .categories .category-card.-fantasy {
+  background: url("../assets/categories/fantasy.png") no-repeat top/cover;
+}
+
+.stories .categories .category-card.-legends-myths {
+  background: url("../assets/categories/legends_myths.png") no-repeat top/cover;
+}
+
+.stories .categories .category-card.-juvenile {
+  background: url("../assets/categories/juvenile.png") no-repeat top/cover;
+}
+
+.stories .categories .category-card.-mystery-police-terror {
+  background: url("../assets/categories/mystery_police_terror.png") no-repeat
+    top/cover;
 }
 </style>
