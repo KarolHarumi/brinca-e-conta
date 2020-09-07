926703442b03e531ba8d045b7c730b6c1f4aa38e
Daniela Rocha Silva <=>
2020-09-07 09:14:42 -0300

    feat: finish footer

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,35 +1,81 @@
 <template>
   <div class="footer">
     <md-bottom-bar md-type="shift">
-      <md-bottom-bar-item to="/home" exact id="bottom-bar-item-home" md-label="Home" md-icon="home"></md-bottom-bar-item>
-      <md-bottom-bar-item to="/home" exact id="bottom-bar-item-search" md-label="Busca" md-icon="search"></md-bottom-bar-item>
-      <md-bottom-bar-item to="/home" exact id="bottom-bar-item-favorite" md-label="Favoritos" md-icon="favorite"></md-bottom-bar-item>
-      <md-bottom-bar-item to="/home" exact id="bottom-bar-item-profile" md-label="Perfil" md-icon="face"></md-bottom-bar-item>
+      <md-bottom-bar-item
+        v-for="(item, index) in icons"
+        :key="index"
+        to="/home"
+        exact
+        :class="{ active: icon === item.icon }"
+        :id="`bottom-bar-item-${item.icon}`"
+        :md-label="item.label"
+        :md-icon="item.icon"
+      ></md-bottom-bar-item>
     </md-bottom-bar>
   </div>
-
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      icons: {
+        home: {
+          label: "Home",
+          icon: "home"
+        },
+        search: {
+          label: "Busca",
+          icon: "search"
+        },
+        favorites: {
+          label: "Favoritos",
+          icon: "favorite"
+        },
+        profile: {
+          label: "Perfil",
+          icon: "face"
+        }
+      }
+    };
+  }
+};
+</script>
+
 <style>
-  .footer {
-    position: fixed;
-    bottom: 0;
-    z-index: 10;
-    left: 0;
-    right: 0;
-  }
+.footer {
+  position: fixed;
+  bottom: 0;
+  z-index: 10;
+  left: 0;
+  right: 0;
+}
 
-  .footer .md-ripple {
-    justify-content: center;
-  }
+.footer .md-ripple {
+  justify-content: center;
+}
 
-  .md-bottom-bar > .md-ripple {
-    flex-wrap: nowrap!important;
-  }
+.md-bottom-bar-item {
+  opacity: 0.6;
+}
 
-  .md-bottom-bar.md-theme-default.md-type-shift {
-    background-color: #78D6FD!important;
-  }
+.md-bottom-bar-item.active {
+  opacity: 1;
+}
 
+.md-bottom-bar > .md-ripple {
+  flex-wrap: nowrap !important;
+}
+
+.md-bottom-bar.md-theme-default.md-type-shift {
+  background-color: #78d6fd !important;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+}
 </style>
