897b7336c03f96469ec415bccad063f82eb82948
Daniela Rocha Silva <=>
2020-09-07 14:23:06 -0300

    feat: add some general rules

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="app">
-    <router-view/>
-    <!--<Footer />-->
+    <router-view />
   </div>
 </template>
 
 <script>
-  //import Footer from '@/components/Footer.vue'
-
-  export default {
-  components: {
-    //Footer
-  }
-}
+export default {};
 </script>
 
 <style>
-  #app {
-    font-family: 'Roboto', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-  }
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-weight: 300;
+}
+
+input {
+  outline: none;
+}
+
+button {
+  border: none;
+  cursor: pointer;
+}
 </style>
