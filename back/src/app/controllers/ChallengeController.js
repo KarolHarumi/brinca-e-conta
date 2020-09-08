251565fdd9ccd19f7c9c7fb4e4de16c87d18964a
Daniela Rocha Silva <=>
2020-09-07 22:30:17 -0300

    feat: return mocked challgens

--- back/src/app/controllers/ChallengeController.js
+++ back/src/app/controllers/ChallengeController.js
@@ -4,7 +4,24 @@ class ChallengeController {
   }
 
   async index(req, res) {
-    return res.json({ message: "Ok" });
+    // get user skills
+    // get story questions
+    // build challenges based on skill levels
+
+    return res.json({
+      challenges: [
+        {
+          question: "Qual personagem que virou a Lua?",
+          answer: "Indiozinho",
+          alternative: "Naiá",
+        },
+        {
+          question: "Qual é a flor preferida da Naiá?",
+          answer: "Rosas",
+          alternative: "Margaridas",
+        },
+      ],
+    });
   }
 }
 
